e4f70d7cab1a1bbaa87446d3dc28cd9a2cb4ebd1
keeepmin94 <wjdwkjh94@gmail.com>
2023-10-11 15:58:50 +0900

    sequelize연결, routes 생성

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 //dotenv.config({ path: "./config/.env" });
 
+import companyRouter from "./routes/company";
+import userRouter from "./routes/user";
+import applicantRouter from "./routes/applicant";
+import noticeRouter from "./routes/notice";
+
 const app = express();
 app.use(express.json());
 
+sequelize
+  .sync()
+  .then(() => console.log("connected database"))
+  .catch((err) => console.error("occurred error in database connecting", err));
+
 app.listen(3000, () => {
-  console.log("백엔드 API 서버가 켜졌어요!!!");
+  console.log("started wanted internship server!");
 });
